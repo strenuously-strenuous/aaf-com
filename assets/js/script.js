c40bb4526c2864a2fc044da3763f4ed6d3d53d79
Ayaan Jain <121211107167@gitarattan.edu.in>
2023-11-23 18:57:58 +0530

    checking design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,37 @@
 // interação do botão de autoriazação do formulário
 
-function activeBox() {
-  if ($(".form label .box").hasClass("active")) {
-    $(".form label .box").removeClass("active");
-  } else {
-    $(".form label .box").addClass("active");
-  }
-}
+// function activeBox() {
+//   if ($(".form label .box").hasClass("active")) {
+//     $(".form label .box").removeClass("active");
+//   } else {
+//     $(".form label .box").addClass("active");
+//   }
+// }
 
-$(".form label .box").on("click", function () {
-  activeBox();
-});
+// $(".form label .box").on("click", function () {
+//   activeBox();
+// });
 
-$(".form label sup").on("click", function () {
-  activeBox();
-});
+// $(".form label sup").on("click", function () {
+//   activeBox();
+// });
 
 // interação do botão de autoriazação do formulário FIM
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1200: {
+      items: 4,
+    },
+  },
+});
