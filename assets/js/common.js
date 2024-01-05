1fd7fcaebc15e6ec8a84a203182a47025c0ff3dc
Ayaan Jain <121211107167@gitarattan.edu.in>
2024-01-05 15:26:54 +0530

    added modal form

---
+++ assets/js/common.js
@@ -0,0 +1,11 @@
+// nav dropdown, open on hover
+// let navDropdown = document.querySelector("nav .dropdown-toggle.nav-link");
+// let navLi = document.querySelector("nav .navbar-nav.mx-auto");
+
+// navLi.addEventListener("mouseover", function () {
+//   console.log("hover");
+//   document.querySelector("nav .dropdown-toggle.nav-link").click();
+// })
+
+// nav dropdown, open on hover
+
